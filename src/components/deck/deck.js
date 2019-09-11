2421f3be43b4071156eb27181accdeae17da0bf7
Illia <illia.zaliatskyi@codeit.com.ua>
2019-09-11 18:26:27 +0300

    changed sctructure of deck and some styles

--- src/components/deck/deck.js
+++ src/components/deck/deck.js
@@ -8,9 +8,12 @@ export default class Deck extends Component {
 
     return (
       <div className="deck">
-        { board.map( card => {
-          return <Card value={card} className="card" key={card} />
-        })}
+        {board.length > 0 && <div className="deck-header">our current deck is:</div>}
+        <div className="deck-cards">
+          { board.map( card => {
+            return <Card value={card} className="card" key={card} />
+          })}
+        </div>
       </div>
     );
   }
