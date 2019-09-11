bef3feca6352fbbd0aad569eef082722efc6a97a
Illia <illia.zaliatskyi@codeit.com.ua>
2019-09-11 14:27:38 +0300

    initial structure commit

---
+++ README.md
@@ -0,0 +1,6 @@
+idp project poker game
+
+steps to run the project:
+
+1. npm install
+2. npm run start
---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './reset.css';
+import './app.css';
+
+const App = () => {
+  return (
+    <main role="main" className="main">
+      <div className="container">
+        initial app
+      </div>
+    </main>
+  );
+};
+
+export default App;
