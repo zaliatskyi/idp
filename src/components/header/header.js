f44450ef756ffe93c462700a50baabcc5a1fce07
Illia <illia.zaliatskyi@codeit.com.ua>
2019-10-24 11:33:21 +0300

    hamburger added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,42 +2,68 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import store from '../../store';
 import './header.scss';
+import './hamburger.scss';
 
 export default class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hamburgerOpened: false
+    }
+  }
+
+  toggleHeader = () => {
+    const { hamburgerOpened } = this.state;
+
+    this.setState((state) => {
+      return {
+        hamburgerOpened: hamburgerOpened ? false : true
+      }
+    });
+  }
+
   render() {
     const { currentPage } = this.props,
+          { hamburgerOpened } = this.state,
           { playerName  } = store.getState();
 
     return (
       <header className="header">
         <nav className="nav">
-          <img src="../logo.svg" className="logo" alt="poker idp logo" />
-          <ul className="nav-list">
-            <li className="nav-list__item">
-              <Link to="/" className="nav-list__link">
-                login page
-              </Link>
-            </li>
-            {
-              currentPage !== 'table' &&
+          <div className="nav__left">
+            <img src="../logo.svg" className="logo" alt="poker idp logo" />
+            <ul className={`nav-list ${ hamburgerOpened ? 'nav-list--active' : '' }`}>
               <li className="nav-list__item">
-                <Link to="/game" className="nav-list__link">
-                  table
+                <Link to="/" className="nav-list__link">
+                  login page
                 </Link>
               </li>
-            }
-            {
-              currentPage !== 'rules' &&
-              <li className="nav-list__item">
-                <Link to="/rules" className="nav-list__link">
-                  rules
-                </Link>
-              </li>
-            }
-          </ul>
+              {
+                currentPage !== 'table' &&
+                <li className="nav-list__item">
+                  <Link to="/game" className="nav-list__link">
+                    table
+                  </Link>
+                </li>
+              }
+              {
+                currentPage !== 'rules' &&
+                <li className="nav-list__item">
+                  <Link to="/rules" className="nav-list__link">
+                    rules
+                  </Link>
+                </li>
+              }
+            </ul>
+          </div>
+          <span className="header__user">Welcome, {playerName}!</span>
+          <div className={`hamburger ${hamburgerOpened ? 'hamburger--active' : ''}`} onClick={ this.toggleHeader }>
+            <div className="hamburger__line hamburger__line--top"></div>
+            <div className="hamburger__line hamburger__line--medium"></div>
+            <div className="hamburger__line hamburger__line--bottom"></div>
+          </div>
         </nav>
-
-        <span className="header__user">Welcome, {playerName}!</span>
       </header>
     )
   }
